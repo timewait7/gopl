9567dcbc54edb2aa20726e71ed1e84cd7fce2b78
linfeng <1029776125@qq.com>
2024-01-06 23:21:56 +0800

    echo2

--- ch1/echo2.go
+++ ch1/echo2.go
@@ -6,12 +6,10 @@ import (
 )
 
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
 }
-
-
