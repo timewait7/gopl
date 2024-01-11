b384d34e2e1a6b1ecbb03eab02b128af0d4466d2
linfeng <1029776125@qq.com>
2024-01-12 00:14:10 +0800

    prints two Fahrenheit-to-Celsius conversions

---
+++ ch2/ftoc.go
@@ -0,0 +1,16 @@
+// Ftoc prints two Fahrenheit-to-Celsius conversions.
+package main
+
+import "fmt"
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g°F = %g°C\n", freezingF, ftoc(freezingF))
+	fmt.Printf("%g°F = %g°C\n", boilingF, ftoc(boilingF))
+}
+
+func ftoc(f float64) float64 {
+	return (f -32) * 5 / 9
+}
+
+
